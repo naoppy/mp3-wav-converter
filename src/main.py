988a9ada887412789f78c3a8a72de59dc7cdd58c
Otani Naoki <e1710@s.akashi.ac.jp>
2020-09-21 02:07:46 +0900

    だいたい実装終了

---
+++ src/main.py
@@ -0,0 +1,67 @@
+import argparse
+import glob
+import os
+
+import ffmpeg
+
+parser = argparse.ArgumentParser(description='指定したフォルダから再帰的にたどり、wavファイルをmp3ファイルに変換する')
+parser.add_argument(
+    'src',
+    metavar='path_to_src',
+    type=str,
+    help='content root folder, both relative path and absolute path are OK.',
+)
+
+parser.add_argument(
+    '-d',
+    default=None,
+    nargs='?',
+    metavar='path_to_out',
+    type=str,
+    help='output folder, both relative path and absolute path are OK.',
+)
+
+
+# 参考：https://qiita.com/suzutsuki0220/items/43c87488b4684d3d15f6
+# ffmpeg -i "input.wav" -vn -ac 2 -ar 44100 -ab 256k -acodec libmp3lame -f mp3 "output.mp3"
+def convert(src, dst):
+    ilist = glob.glob(src + '/**/*.wav', recursive=True)
+    for filename in ilist:
+        filename = os.path.abspath(filename)
+        relative = os.path.relpath(filename, src)
+        outpath = os.path.splitext(os.path.join(dst, relative))[0] + '.mp3'
+        # print(filename)
+        # print(relative)
+        # print(outpath)
+        input = ffmpeg.input(filename)
+        out = ffmpeg.output(input, outpath,
+                            vn=True,
+                            # ac='2',
+                            # ar='44100',
+                            ab='128k',
+                            acodec='libmp3lame',
+                            f='mp3'),
+        ffmpeg.run(out)
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    src = args.src
+    dst = args.d
+
+    if not os.path.exists(src):
+        print("broken src path")
+        exit(1)
+
+    if dst is not None:
+        if not os.path.exists(dst):
+            print("broken dst path")
+            exit(1)
+    else:
+        dst = os.getcwd()
+
+    src = os.path.abspath(src)
+    dst = os.path.abspath(dst)
+    print('src dir :' + src)
+    print('dst dir :' + dst)
+    convert(src, dst)
